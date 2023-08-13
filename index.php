662b98c4be3043ccb204173ccc1d967b8b074818
Michael Becker <21beckem8@gmail.com>
2023-08-13 18:46:15 +0200

    added login page and VERY simple security

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
+<?php
+    session_start();
+    if ($_POST['username'] && $_POST['password']) {
+
+    } else {
+        if (!$_SESSION['missionSignedIn']) {
+            header('location: login.html');
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="admin-template.css">
     <script src="https://kit.fontawesome.com/0bddc0a0f7.js" crossorigin="anonymous"></script>
     <title>Referral Panel</title> 
 </head>
