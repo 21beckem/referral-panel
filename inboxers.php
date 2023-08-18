e624112af4b9b97ca622680fb020fc61ea6ef114
Michael Becker <21beckem8@gmail.com>
2023-08-18 19:32:49 +0200

    making it all editable. still in progress

--- inboxers.php
+++ inboxers.php
@@ -12,14 +12,34 @@
     $teamInfos = readSQL($_SESSION['missionInfo']->mykey, 'SELECT * FROM `teams` WHERE 1');
 ?>
 <style>
-#inboxersParent {
+#inboxersParentParent {
+    margin-left: auto;
+    margin-right: auto;
     position: relative;
     min-width: 600px;
     width: 100%;
-    max-width: 900px;
+    max-width: 1200px;
+}
+#inboxersParent {
+    width: 100%;
     border-spacing: 0 10px;
 }
-#inboxersParent tr:not(:first-child):not(:last-child) {
+#inboxersParent tr.editing input, #inboxersParent tr.editing select {
+    cursor: auto;
+    pointer-events: auto;
+}
+#inboxersParent tr.editing .editBtn {
+    display: none;
+}
+#inboxersParent tr:not(.editing) .saveBtn {
+    display: none;
+}
+#inboxersParent tr.disabled {
+    cursor: default;
+    pointer-events: none;
+    opacity: 0.5;
+}
+#inboxersParent tr:not(:first-child) {
     box-shadow: 2px 5px 10px -7px rgba(0, 0, 0, 0.5);
 }
 #inboxersParent tr:first-child td {
@@ -33,7 +53,13 @@
     right: 10px;
     padding: 5px 10px;
 }
+.editBtn, .saveBtn, .delBtn {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
 input[type=text], select {
+    pointer-events: none;
     background-color: #f9f9f9;
     border: none;
     padding: 5px;
@@ -47,42 +73,41 @@ input[type=text], select {
 </div>
 
 <div class="dash-content">
-    <center>
-        <table id="inboxersParent">
-            <tr>
-                <td>Area Name</td><td>Email</td><td>Color</td><td>Role</td><td></td>
-            </tr>
-            <tr style="background-color: cyan;">
-                <td><input type="text" value="Hägersten"></td>
-                <td><input type="text" value="1234567@missionary.org"></td>
-                <td><select><option>Red</option></select></td>
-                <td><select><option>Standard</option></select></td>
-                <td><i class="fa-solid fa-trash-can"></i></td>
-            </tr>
-            <tr style="background-color: red;">
-                <td><input type="text" value="Hägersten"></td>
-                <td><input type="text" value="1234567@missionary.org"></td>
-                <td><select><option>Red</option></select></td>
-                <td><select><option>Standard</option></select></td>
-                <td><i class="fa-solid fa-trash-can"></i></td>
-            </tr>
-            <tr>
-                <td colspan="999">
-                    <button id="addNewTeam" class="purpleBtn">Add New Team</button>
-                </td>
-            </tr>
-        </table>
-    </center>
-    <form action="saving_functions/schedule_save.php" method="POST">
-        <input type="hidden" id="hiddenSavingEl" name="saveIt">
-        <input type="submit" id="saveBtn" class="purpleBtn" onclick="bigSaveBtn()" value="Save to Referral Suite">
-    </form>
+        <div id="inboxersParentParent">
+            <table id="inboxersParent">
+                <tr>
+                    <td>Area Name</td><td>Email</td><td>Color</td><td>Role</td><td></td>
+                </tr>
+                <tr style="background-color: cyan;">
+                    <td><input type="text" value="Hägersten"></td>
+                    <td><input type="text" value="1234567@missionary.org"></td>
+                    <td><select><option>Red</option></select></td>
+                    <td><select><option>Standard</option></select></td>
+                    <td><i class="fa-solid fa-trash-can"></i></td>
+                </tr>
+                <tr style="background-color: red;">
+                    <td><input type="text" value="Hägersten"></td>
+                    <td><input type="text" value="1234567@missionary.org"></td>
+                    <td><select><option>Red</option></select></td>
+                    <td><select><option>Standard</option></select></td>
+                    <td><i class="fa-solid fa-trash-can"></i></td>
+                </tr>
+            </table>
+            <br>
+            <button id="addNewTeam" class="purpleBtn">Add New Team</button>
+            <br>
+            <form id="hiddenForm" action="saving_functions/inboxers_save.php" method="POST">
+                <input type="hidden" id="hiddenSavingEl" name="saveIt">
+            </form>
+        </div>
     <script>
 function _(x) { return document.getElementById(x); }
+HTMLCollection.prototype.forEach = function (x) { return Array.from(this).forEach(x); }
 const InboxColors = <?php echo(json_encode($InboxColors)); ?>;
 let teamInfos = <?php echo(json_encode($teamInfos)) ?>;
 
 let inboxersParent = _('inboxersParent');
+let hiddenForm = _('hiddenForm');
 
 // make team color lookup
 let teamColorLookup = {};
@@ -120,11 +145,27 @@ function makeInboxersList() {
                 <td><input type="text" value="`+tm[2]+`"></td>
                 <td><select>`+clrList+`</select></td>
                 <td><select>`+rolList+`</select></td>
-                <td><i class="fa-solid fa-trash-can"></i></td>
+                <td>
+                    <button class="editBtn" onclick="enableEdits(this.parentElement.parentElement)"><i class="fa-solid fa-pen-to-square"></i></button>
+                    <button class="saveBtn" onclick="saveEdits(`+i+`)"><i class="fa-solid fa-floppy-disk"></i></button>
+                </td>
+                <td><button class="delBtn"><i class="fa-solid fa-trash-can"></i></button></td>
             </tr>`;
     }
-    toPaste += '<tr><td colspan="999"><button id="addNewTeam" class="purpleBtn">Add New Team</button></td></tr>';
     inboxersParent.innerHTML = toPaste;
+}
+function enableEdits(tr) {
+    document.querySelectorAll('#inboxersParent tr').forEach(x => {
+        x.classList.remove('editing');
+        if (x != tr) {
+            x.classList.add('disabled');
+        }
+    });
+    tr.classList.add('editing');
+    console.log(tr);
+}
+function saveEdits(tr, tmId) {
+
 }
 makeInboxersList();
     </script>
