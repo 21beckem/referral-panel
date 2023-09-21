18f71184a43273fb8e1e3bdd531dde046b3765ac
Michael Becker <21beckem8@gmail.com>
2023-09-21 16:32:36 +0200

    made temp place for tutorial videos on dashboard

--- index.php
+++ index.php
@@ -39,66 +39,23 @@
             </div>
         </div>
     </div>
+    <br><br>
 
-    <div class="activity">
+    <div class="overview">
         <div class="title">
-            <i class="fa-solid fa-clock"></i>
-            <span class="text">Recent Activity</span>
+            <i class="fa-solid fa-graduation-cap"></i>
+            <span class="text">Tutorials</span>
         </div>
 
-        <div class="activity-data">
-            <div class="data names">
-                <span class="data-title">Name</span>
-                <span class="data-list">Prem Shahi</span>
-                <span class="data-list">Deepa Chand</span>
-                <span class="data-list">Manisha Chand</span>
-                <span class="data-list">Pratima Shahi</span>
-                <span class="data-list">Man Shahi</span>
-                <span class="data-list">Ganesh Chand</span>
-                <span class="data-list">Bikash Chand</span>
-            </div>
-            <div class="data email">
-                <span class="data-title">Email</span>
-                <span class="data-list">premshahi@gmail.com</span>
-                <span class="data-list">deepachand@gmail.com</span>
-                <span class="data-list">prakashhai@gmail.com</span>
-                <span class="data-list">manishachand@gmail.com</span>
-                <span class="data-list">pratimashhai@gmail.com</span>
-                <span class="data-list">manshahi@gmail.com</span>
-                <span class="data-list">ganeshchand@gmail.com</span>
-            </div>
-            <div class="data joined">
-                <span class="data-title">Joined</span>
-                <span class="data-list">2022-02-12</span>
-                <span class="data-list">2022-02-12</span>
-                <span class="data-list">2022-02-13</span>
-                <span class="data-list">2022-02-13</span>
-                <span class="data-list">2022-02-14</span>
-                <span class="data-list">2022-02-14</span>
-                <span class="data-list">2022-02-15</span>
-            </div>
-            <div class="data type">
-                <span class="data-title">Type</span>
-                <span class="data-list">New</span>
-                <span class="data-list">Member</span>
-                <span class="data-list">Member</span>
-                <span class="data-list">New</span>
-                <span class="data-list">Member</span>
-                <span class="data-list">New</span>
-                <span class="data-list">Member</span>
-            </div>
-            <div class="data status">
-                <span class="data-title">Status</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-                <span class="data-list">Liked</span>
-            </div>
-        </div>
+        <center>
+            <iframe width="424" height="238"
+            src="https://www.youtube.com/embed/BDyCvzzOKhc"
+            title="Kan Gud ge mig hjälp?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style="border-radius: 10px;"></iframe>
+        </center>
     </div>
+
 </div>
 
 <?php makeHTMLbottom() ?>
