5d30719e12462a8218ef3207aa30a888966228ce
Michael Becker <21beckem8@gmail.com>
2023-09-23 09:10:04 +0200

    accordions now populated with headers and names

--- settings.php
+++ settings.php
@@ -24,7 +24,7 @@
     transition: 0.4s;
 }
 .settingsSection.active, .settingsSection .openBtn:hover {
-    background-color: #ccc;
+    background-color: #ebdcf0;
 }
 .settingsSection .openBtn:after {
     content: '\002B';
@@ -37,11 +37,24 @@
     content: "\2212";
 }
 .settingsSection .panel {
-    padding: 0 18px;
+    padding: 0px;
     background-color: white;
     max-height: 0;
     overflow: hidden;
-    transition: max-height 0.2s ease-out;
+    transition: all 0.2s ease-out;
+}
+.settingsSection.active .panel {
+    padding: 20px 10px;
+}
+
+.settingName {
+    font-size: 20px;
+    margin-right: 10px;
+}
+.settingSplit {
+    width: 75%;
+    margin: 15px auto 15px auto;
+    border: 1px dotted #d2d2d2;
 }
 </style>
 <div class="top">
@@ -51,7 +64,7 @@
 </div>
 
 <div class="dash-content">
-    <div>
+    <div id="accordionParent">
         <div class="settingsSection">
             <button class="openBtn" onclick="clickOpenAccordion(this)">Section 1</button>
             <div class="panel">
@@ -79,6 +92,46 @@
 <script>
 function _(x) { return document.getElementById(x); }
 HTMLCollection.prototype.forEach = function (x) { return Array.from(this).forEach(x); }
+const settings_rows = <?php echo(json_encode( $settings_rows )); ?>;
+
+function makeAccordions(this_settings_rows) {
+    let lastHeader = null;
+    let accordions = this_settings_rows.map((row, i) => {
+        let toReturn = '';
+        
+        // start new header section
+        if (lastHeader != row[2]) {
+            if (lastHeader != null) {
+                toReturn += '</div></div>';
+            }
+            toReturn += `<div class="settingsSection">
+                            <button class="openBtn" onclick="clickOpenAccordion(this)">` + row[2] + `</button>
+                        <div class="panel">`;
+            lastHeader = row[2];
+        }
+
+        // paste name
+        toReturn += '<a class="settingName">' + row[5] +'</a>';
+
+        // make value editor
+        let typ = row[3];
+        if (typ=='text' || typ=='number' || typ=='date') {
+            toReturn += `<input type="`+typ+`" disabled value="`+row[6]+`">`;
+        }
+        
+        // add break between
+        if (this_settings_rows[i+1] && this_settings_rows[i+1][2]==row[2]) {
+            toReturn += '<hr class="settingSplit">';
+        }
+        return toReturn;
+    }).join('');
+    if(accordions != '') {
+        accordions += '</div></div>';
+    }
+
+    _('accordionParent').innerHTML = accordions;
+}
+makeAccordions( settings_rows );
 
 function clickOpenAccordion(el) {
     // close all others
@@ -103,7 +156,7 @@ function openAccordion(el, forceState=null) {
     }
     let panel = el.nextElementSibling;
     if (el.parentElement.classList.contains('active')) {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
+        panel.style.maxHeight = (panel.scrollHeight+40) + 'px';
     } else {
         panel.style.maxHeight = '0';
     }
