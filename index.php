1deaf0bb4ccadee8031893788183864b1a85d40a
Michael Becker <21beckem8@gmail.com>
2023-10-03 09:00:22 +0200

    added coming soon stuff on dashboard

--- index.php
+++ index.php
@@ -21,21 +21,10 @@
             <span class="text">Dashboard</span>
         </div>
 
-        <div class="boxes">
-            <div class="box box1">
-                <i class="fa-solid fa-plus"></i>
-                <span class="text">Referrals Received Today</span>
-                <span class="number">0</span>
-            </div>
-            <div class="box box2">
-                <i class="fa-solid fa-clock"></i>
-                <span class="text">Avg Claim Time</span>
-                <span class="number">0 min</span>
-            </div>
-            <div class="box box3">
-                <i class="fa-solid fa-paper-plane"></i>
-                <span class="text">Percent Sent</span>
-                <span class="number">0%</span>
+        <div class="boxes" style="justify-content:center">
+            <div class="box" style="width:100%; background-color:#00000029; opacity:0.3; max-width:900px">
+                <span class="text">Coming Soon</span>
+                <span class="number">Quick Stats</span>
             </div>
         </div>
     </div>
@@ -46,14 +35,21 @@
             <i class="fa-solid fa-graduation-cap"></i>
             <span class="text">Tutorials</span>
         </div>
+        <div class="boxes" style="justify-content:center">
+            <div class="box" style="width:100%; background-color:#00000029; opacity:0.3; max-width:900px">
+                <span class="text">Coming Soon</span>
+                <span class="number">Tutorial Videos</span>
+            </div>
+        </div>
 
-        <center>
+        
+        <!-- <center>
             <iframe width="424" height="238"
             src="https://www.youtube.com/embed/BDyCvzzOKhc"
             title="Kan Gud ge mig hjälp?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
             style="border-radius: 10px;"></iframe>
-        </center>
+        </center> -->
     </div>
 
 </div>
